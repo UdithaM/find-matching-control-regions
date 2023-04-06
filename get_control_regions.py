b2a91770025be8d200f5d6f39ec8e24306df9836
udithamaduranga <u242509@bcm.edu>
2023-04-06 14:05:12 -0500

    changes to argumants and iterations

--- get_control_regions.py
+++ get_control_regions.py
@@ -4,16 +4,32 @@ import sys
 import os
 import time
 from pandas.io.common import EmptyDataError
+import argparse
+ 
+parser = argparse.ArgumentParser(description="--------Get Control Regions For CoRSIV Regions--------",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("-g", "--genic", action="store_true", help="Whether genic control regions are needed. Make sure that gene file is provided with if this is set to True. Use -f or --gene_file to provide gene file.")
+parser.add_argument("-f", "--gene_file",  help="Path to file containing genes in whole genome in BED format. Columns of gene file should be file chr, start, end, ... and should be in 1-based coordinates. Column headers should not be in the bed file.")
+parser.add_argument("Annotation_File", help="Path to annotation file (CSV) for a given chromosome.")
+parser.add_argument("Annotation_File_Bin_Size", help="Bin Size of the annotation file provided. ( ex: 100, 200)")
+parser.add_argument("CoRSIV_File", help="Path to CoRSIV file (CSV) for a given chromosome. Columns of CoRSIV file should be file chr, start, end, cpg_count, id, ... and should be in 1-based coordinates")
+parser.add_argument("Chromosome", help="Chromosome in the format chrx.")
+parser.add_argument("Output_File", help="Path of the output file containing control regions.")
+args = parser.parse_args()
+config = vars(args)
+if (config['genic'] and config['gene_file'] is None):
+    print("ERROR: Please enter the path to file containing genes in whole genome in BED format. Columns of gene file should be file chr, start, end, ... and should be in 1-based coordinates. Column headers should not be in the bed file. ")
 
 
 
 # parameters
-annotation_filename = sys.argv[1] 
-corsiv_filename = sys.argv[2]
-output_filename = sys.argv[3]
-# get chromosome in the type 'chr4'
-chromosome = sys.argv[4]
-genic = sys.argv[5]
+annotation_filename = config['Annotation_File'] 
+annotation_bin_size = int(config['Annotation_File_Bin_Size'])
+corsiv_filename = config['CoRSIV_File']
+output_filename = config['Output_File']
+chromosome = config['Chromosome']
+genic = config['genic']
+gene_filename = config['gene_file']
 
 # # parameters
 # annotation_filename = "../../Annotations/chr21_annotated.csv"
@@ -38,7 +54,7 @@ def find_match(cpg, size, bin_dict):
     done = False
     count = 0
     # if after 200 times we still can't find a control, we will loosen up the requirement and try again, can change 200 to whatever number
-    while not done and count <= 200:
+    while not done and count <= 400:
         # randomly pick a bin
         coord, bin_cpg = random.choice(list(bin_dict.items()))
         # start at a bin with cpg number > 0
@@ -47,7 +63,7 @@ def find_match(cpg, size, bin_dict):
             cpg_num = 0
             # flag is used to mark if exception is raised
             flag = 0
-            for i in range(coord, coord+size, 100):
+            for i in range(coord, coord+size, annotation_bin_size ):
                 # if the start bin we pick happens to end at a corsiv region and results in key error, we try again
                 try:
                     cpg_num += bin_dict[i]
@@ -56,22 +72,27 @@ def find_match(cpg, size, bin_dict):
                     break
            
             # return if we find one
-            if ((cpg_num == cpg) and (not flag) and (genic == 'genic')):
+            if ((cpg_num == cpg) and (not flag) and genic):
                 # bedtools command to check whether there are any genes overlapping 3kb upstream or downstream region
                 # including control region itself
                 end_coordinate = (coord + size - 1) 
                 data = {'chr': [chromosome],'start': [coord - 3000],'end': [end_coordinate + 3000]}
-                # Create DataFrame
+                # Create a dataFrame for bedtools intersect including control region
                 temp_df = pd.DataFrame(data)
-                control_region_file_name = 'chrom_21_control/control_region_for_%s_%s_%s.bed' % (chromosome, coord, end_coordinate)
-                gene_overlap_file_name = 'chrom_21_control/control_region_for_%s_%s_%s_gene_overlap.bed' % (chromosome, coord, end_coordinate)
+                genic_folder = '%s_genic'  % chromosome
+                # Create a folder for bedtools
+                os.system('mkdir -p %s' % genic_folder)
+                time.sleep(2)
+                control_region_file_name = '%s/control_region_for_%s_%s_%s.bed' % (genic_folder,chromosome, coord, end_coordinate)
+                gene_overlap_file_name = '%s/control_region_for_%s_%s_%s_gene_overlap.bed' % (genic_folder,chromosome, coord, end_coordinate)
                 temp_df.to_csv(control_region_file_name , sep='\t', header=False, index=False)
-                print("SAVED!")
                 time.sleep(3)
-                os.system('bedtools intersect -a %s -b 100bp_annotations/every_known_gene.bed -wb -wa > %s' % (control_region_file_name, gene_overlap_file_name))
+                # Check for overlaps with genes within 3kb upstream and downstream
+                os.system('bedtools intersect -a %s -b %s -wb -wa > %s' % (control_region_file_name, gene_filename, gene_overlap_file_name))
 #                 !bedtools intersect -a {control_region_file_name} -b corsiv_100kb_split1000/every_known_gene.bed -wb -wa > {gene_overlap_file_name}
                 time.sleep(3)
                 try:
+                    # If genic return matching genic control region
                     gene_overlap_df = pd.read_csv(gene_overlap_file_name, header=None, sep="\t")
                     done = True
                     return coord, coord + size - 1
@@ -98,7 +119,7 @@ bin_cpg_num = {}
 
 # fills up the dictionary
 for idx, row in annotation.iterrows():
-    coord = int(row['Bin Name'].split('_')[1]) - 99
+    coord = int(row['Bin Name'].split('_')[1]) - (annotation_bin_size -1)
     bin_cpg_num[coord] = row['CpG Count']
 
 print(bin_cpg_num)
@@ -107,9 +128,6 @@ print(bin_cpg_num)
 # should be 1-based coordinates
 corsiv_df = pd.read_csv(corsiv_filename)
 corsiv_df = corsiv_df.loc[corsiv_df['chr'] == chromosome]
-# get each row corresponds to the first bin in each CoRSIV
-# corsiv_df['first_bin_end'] = corsiv_df['start'] + 99
-# corsiv_first_bin = corsiv_df[['chr','start', 'first_bin_end', 'cpg_count', 'id']]
 
 # stores in the form of (start coord, end coord, cpg num) for each CoRSIV
 corsiv_cpg_num = []
@@ -122,11 +140,6 @@ for idx, row in corsiv_df.iterrows():
     id_ = row['id']
     corsiv_cpg_num.append((start, end, cpg_num, id_))
 
-# delete known corsivs from the dictionary
-# for block in corsiv_cpg_num:
-#     for num in range(block[0], block[1], 100):
-#         del bin_cpg_num[num]
-
 
 block_id = []
 start_coord = []
@@ -134,14 +147,14 @@ end_coord = []
 exact_match = []
 
 for block in corsiv_cpg_num:
-    # attempt 1-200, match = 1 means an exact match and match = 0 means an approximate
+    # attempt 1-400, match = 1 means an exact match and match = 0 means an approximate
     match = 1
     result = find_match(block[2], block[1]-block[0]+1, bin_cpg_num)
-    # attempt 201-400
+    # attempt 401-800
     if not result:
         result = find_match(block[2], block[1]-block[0]+101, bin_cpg_num)
         match = 0
-    # attempt 401-600
+    # attempt 801-1200
     if not result:
         result = find_match(block[2], block[1]-block[0]+201, bin_cpg_num)
         match = 0
